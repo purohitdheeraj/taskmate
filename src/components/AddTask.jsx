0067edd06b11e14e9cec0088b4b4083efb8988ca
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-07-06 18:28:04 +0530

    feat : add input component and basic stye

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const AddTask = () => {
-	return <section></section>;
+	return (
+		<section className="addTask flex-container">
+			<input
+				className="taskInput"
+				type="text"
+				placeholder="add task"
+				autoComplete="off"
+				maxLength={24}
+			/>
+			<button className="btn" type="submit">
+				Add
+			</button>
+		</section>
+	);
 };
--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const ShowTask = () => {
-	return <div>ShowTask</div>;
+	return (
+		<section className="showTask">
+			<div className="heading">
+				<div className="title">
+					<h2 className="text">Todo</h2>
+					<span className="count">0</span>
+				</div>
+
+				<button className="btn btn-clearAll">
+					Clear All
+				</button>
+			</div>
+		</section>
+	);
 };
